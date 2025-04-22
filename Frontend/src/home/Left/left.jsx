8682a8cd08224c5363b0ed1bd930163152a46569
ANISH-SINGH <anishraaz90@gmail.com>
2025-04-22 18:44:24 +0530

    tailwind setup

---
+++ Frontend/src/home/Left/left.jsx
@@ -0,0 +1,9 @@
+function Left() {
+  return (
+    <div className="w-[30%] border border-white bg-black text-white">
+        left
+    </div>
+  )
+}
+
+export default Left;
---
+++ Frontend/src/home/Right/right.jsx
@@ -0,0 +1,9 @@
+function Right() {
+  return (
+    <div className="w-[70%] border">
+      Right
+    </div>
+  )
+}
+
+export default Right;
